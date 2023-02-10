63346ef5ce002b9486d603fb26c91a90f215b9c6
Ta0Huu <anuwatch6552@gmail.com>
2023-02-10 16:03:46 +0700

    saved

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,43 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+
+void showData(double *x,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(x+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (rand()%101)*0.01;
+    }
+}
+
+void findRowSum(const double *x,double *y,int N,int M){
+    int k = 0;
+    *(y+k) = 0; 
+    cout << fixed << setprecision(2);
+    for(int i = 0;i < N*M;i++){
+        *(y+k) += *(x+i);
+        if((i+1)%M == 0){
+            k++;
+            *(y+k) = 0;
+        }
+        
+    }
+}
+
+void findColSum(const double *x,double *y,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0;i < M;i++){
+        *(y+i) = 0;
+        for(int j = i;j < N*M;j=j+M){
+            *(y+i) += *(x+j);
+        }
+    }
+}
